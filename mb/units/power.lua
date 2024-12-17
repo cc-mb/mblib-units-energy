f71d8c5bf2fdcc2a95d22603945f275dd019e985
Milan Borovy <milan.borovy@bisimulations.com>
2024-12-17 10:57:06 +0100

    Add prefixes to W

--- mb/units/power.lua
+++ mb/units/power.lua
@@ -1,10 +1,20 @@
 local Type = require "mb.units.base.type"
 
+local Prefix = require "mb.units.prefix"
 local Energy = require "mb.units.energy"
 local Time = require "mb.units.time"
 
 --- Power units.
 local Power = Type.new()
+
+--- Apply prefxes to the unit.
+---@param unit string
+---@param prefixes string[]
+local function apply_prefixes(unit, prefixes)
+  for _, prefix in ipairs(prefixes) do
+    Power:register(Prefix[prefix] * Power[unit])
+  end
+end
 
 --- Units cross product.
 ---@param a string[] Energy units.
@@ -23,5 +33,6 @@ cross_product({"J", "EU", "KiEU", "MiEU", "GiEU", "TiEU", "RF"}, {"t"})
 -- Define Watt as FE/s instead of J/s as FE is the standard unit.
 Power:register(Power["FE/s"] .. "W")
 Power:register(Power["W"] / 745.7 .. "hp")
+apply_prefixes("W", {"k", "M", "G", "T"})
 
 return Power
